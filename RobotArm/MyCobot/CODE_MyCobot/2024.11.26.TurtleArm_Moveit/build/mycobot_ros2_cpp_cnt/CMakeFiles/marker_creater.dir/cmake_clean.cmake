ba33b25ede996b164aa1c80cb318535bbcf2197a
Seyeon1225 <angelo061225@gmail.com>
2024-12-05 10:06:27 +0900

    Fix commit

---
+++ RobotArm/MyCobot/CODE_MyCobot/2024.11.26.TurtleArm_Moveit/build/mycobot_ros2_cpp_cnt/CMakeFiles/marker_creater.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/marker_creater.dir/src/marker_creater.cpp.o"
+  "CMakeFiles/marker_creater.dir/src/marker_creater.cpp.o.d"
+  "marker_creater"
+  "marker_creater.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/marker_creater.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RobotArm/MyCobot/CODE_MyCobot/2024.11.26.TurtleArm_Moveit/build/mycobot_ros2_cpp_cnt/CMakeFiles/marker_creater.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for marker_creater.
+# This may be replaced when dependencies are built.
